7186f9c70bb9a40f9584a718960067d3db80ce46
Yusuf Tufan <ytfn7272@gmail.com>
2025-08-11 12:30:11 +0300

    Updated the project

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ window=Tk()
 window.title("The Scret Notes App")
 window.config(padx=10,pady=80)
 window.minsize(500,600)
-window.config(bg='white')
+window.config(bg='silver')
 
 
 #image
@@ -34,7 +34,7 @@ resized=img.resize((150,150),Image.Resampling.LANCZOS)
 itk=ImageTk.PhotoImage(resized)
 lbl=Label(window,image=itk)
 lbl.image=itk
-lbl.pack()
+lbl.pack(pady=(0,20))
 
 #create file
 def save_and_encypt():
@@ -63,26 +63,35 @@ def save_and_encypt():
 
 def decrypted_note():
     message_encrypted = text.get('1.0',END)
-    user_key =enter_key.get()
-
-    if len(message_encrypted) == 0 or len(user_key)==0:
+    
+    password=passw_var.get()
+    passw_var.set("")
+    
+    if len(message_encrypted) == 0 or len(password)==0:
         messagebox.showinfo(title='Error',message='Please enter all info.')
 
     else:
         try:
-            decrypted_message = decode(user_key, message_encrypted)
+            decrypted_message = decode(password, message_encrypted)
             text.delete('1.0', END)
             text.insert('1.0', decrypted_message)
+            
         except:
             messagebox.showinfo(title='Error',message='Please encrypted text')
+        enter_key.delete(0,END)
 
+def clear_all():
+    text.delete(1.0,END)
+    enter_key.delete(0,END)
+    enter_title.delete(0,END)
+ 
 #title
 title=Label(text='Enter Your File Name',font=("Arial",10,'bold'))
 title.pack()
 
 #enter title
 enter_title=Entry(width=35)
-enter_title.pack()
+enter_title.pack(pady=(0,20))
 
 #text title
 text_title=Label(text='Enter Your Secret Notes',font=("Arial",10,'bold'))
@@ -91,15 +100,16 @@ text_title.pack()
 #enter text
 text=Text(width=43,height=15)
 text.config(font=("Arial",10,'italic'))
-text.pack()
+text.pack(pady=(0,20))
 
 #title key
 key_title=Label(text="Enter Master Key",font=("Arial",10,'bold'))
 key_title.pack()
+passw_var=StringVar()
 
 #enter key
-enter_key=Entry(width=35)
-enter_key.pack()
+enter_key=Entry(width=35,textvariable=passw_var,show='*')
+enter_key.pack(pady=(0,20))
 
 #save & encrypt
 save_encrypt=Button(text="Save & Encrypt",font=("Arial",10,'bold'),command=save_and_encypt)
@@ -109,6 +119,10 @@ save_encrypt.pack()
 #decrypt
 decrypt=Button(text="Decrypt",font=("Arial",10,'bold'),command=decrypted_note)
 decrypt.pack()
+
+#clear the window
+clear_button = Button(text="Clear ALL",bg='firebrick',command=clear_all)
+clear_button.pack(pady=10)
 
 
 #mainloop
